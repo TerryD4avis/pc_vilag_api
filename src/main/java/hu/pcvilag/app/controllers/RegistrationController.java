e95d1a87006345dc7e31ab953a74f448b5b88b10
Jeno Mario Varga <baphomet14@gmail.com>
2023-11-19 12:03:20 +0100

    fix: password stored as plain text

--- src/main/java/hu/pcvilag/app/controllers/RegistrationController.java
+++ src/main/java/hu/pcvilag/app/controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import hu.pcvilag.app.models.UserEntity;
 import hu.pcvilag.app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,14 @@ public class RegistrationController {
     @Autowired
     private UserService userService;
 
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserEntity user) {
         if (userService.usernameExists(user.getUsername())) {
             return ResponseEntity.badRequest().body("User with this name already exists");
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         userService.registerNewUser(user);
         return ResponseEntity.ok("User registered successfully");
     }
